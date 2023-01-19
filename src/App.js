ff0f328e8c84ca67385d8d67ca7cd72f8e8df5f0
Prajwal Patil <psplrock2000@gmail.com>
2023-01-19 15:42:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState("");
+  const [result, setResult] = useState("");
+
+  const input1 = document.getElementById("w");
+  const input2 = document.getElementById("h");
+
+  function unit() {
+    let x = document.getElementById("opt");
+  }
+
+  function click(event) {
+    event.preventDefault();
+
+    if (!weight || !height) {
+      alert("Enter the both Input Values");
+    } else {
+      let x = document.getElementById("opt").value;
+      if (x === "cm") {
+        let cal = (input1.value / (input2.value / 100) ** 2).toFixed(1);
+        setBmi(cal);
+
+        if (cal < 18.5) {
+          setResult("You are Under-weight");
+        } else if (cal >= 18.5 && cal < 25) {
+          setResult("You are Normal");
+        } else {
+          setResult("You are Overweight");
+        }
+        
+      } else {
+        let cal = (input1.value / (input2.value * 0.304) ** 2).toFixed(1);
+        setBmi(cal);
+
+        if (cal < 18.5) {
+          setResult("You are Under-weight");
+        } else if (cal >= 18.5 && cal < 25) {
+          setResult("You are Normal");
+        } else {
+          setResult("You are Overweight");
+        }
+      }
+    }
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><u>Bmi Calculator </u></h1>
+      <br />
+
+      <form onSubmit={click}>
+        <label htmlFor="weight">Weight : </label>
+        <input
+          type="number"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+          id="w"
+        />
+        <br />
+
+        <br />
+        <label htmlFor="height">Height : </label>
+        <input
+          type="number"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+          id="h"
+        />
+
+        <select name="unit" id="opt" onChange={unit}>
+          <option value="">Select</option>
+          <option value="cm">cm</option>
+          <option value="feet">Feet</option>
+        </select>
+        <br />
+
+        <br />
+        <button id="btn">Calculate</button>
+      </form>
+      <br />
+      <div id="child">
+        <p>BMI : <b>{bmi}</b></p>
+        <p id="add"><u>{result}</u></p>
+      </div>
     </div>
   );
 }
